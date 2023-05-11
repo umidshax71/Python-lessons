fee01b69dc0b2825bf8511329e0817e6ce55fecf
umidshax71 <ismoilovumid609@gmail.com>
2023-05-11 09:39:14 +0300

    deploy

---
+++ dars1.py
@@ -0,0 +1,296 @@
+# a = "Assalomu alaykum men yangi avlod zamonaviy dasturlashning, Python dasturlash intlektiman "
+# print(a)
+
+
+# B = "Python dasturlash tili 1991-yil Gallandyalik dasturlovchi Gvido Van Rossum Tomonidan ishlab chiqarligan bulib  hozir ham yangi texnalogiyalarni ishlab chiqarib top o'rinlarni egallab kelmoqda "
+# print(B)
+
+
+# a = 127
+# b = 73
+# print(a+b)
+
+# g = "Tortburchat topish formulasi: "
+# h = "P=2*(a+b)"
+# print(f"{g} {h}")
+
+
+
+
+
+# a = 321
+# b = 73
+# h = a+b
+# n = a-b
+# print(f"yig'indisi {h}\nayrmasi {n}")
+
+
+# a = "Algaritm bu - birxil ketma ketlikda biror muammoni bosqichma bosqich tartibda yechim berish kurinishidir "
+# print(a)
+
+# a = 299
+# b = 199
+# h = a+b
+# k = a-b
+# l = a*b
+# f = a/b
+# print(f"yig'indi: {h}\nayirma: {k}\nkupaytma: {l}\nbo'linma: {f}")
+
+#
+# a = "Dasturlash bu -biror muammoni zamonaviy texnalogiyalar yordamida hal etish uchun ishlatiladi gan yunalishlardir. Dasturlash 2 turga bulinadin 1- front end backend.\n Frontend bu - biz dasturda kurib turgan yozuvlar dezaynlar , Backend esa - ma'lumotlar bilan ishlay digan kurinishdir "
+# print(a)
+
+
+
+
+
+
+
+
+
+#1
+# A =  int(input("Tug'ilgan yiling gizni kiriting: "))
+# b= 2023-A
+# print(f"Bu sizning yoshing giz: {b}")
+
+
+
+# 2)a = int(input("yoshinggizni kiriting:"))
+# b = 2023-a
+# print(f"Bu sizning yilinggiz:{b}")
+
+
+
+#3
+# a = int(input("Aylananing radyusini kiriting:"))
+# b =2*a
+# print(f"Bu rdyus {a}\nDyametri {b} " )
+
+
+# 4)A = input("Isming gizni kiriting:")
+# print(f"Assalomu alaykum {A} platformaga hush kelibsiz ")
+
+
+
+# 5
+# a = int(input("istalgan sonni kiriting:"))
+# b=a-a
+# print(f"Barchasini javobi {b}")
+
+
+
+
+
+#6
+# a = int(input("istalgan natural son kiriting:"))
+# b = -a
+# print(b)
+
+
+
+
+
+# 8
+# a = 0
+# b= -(a+1)
+# print(b)
+
+
+# 9
+# a = int(input("Sm kiriting mm ga aylantraman:"))
+# b = a*10
+# print(f"{a}sm teng {b}mm")
+
+
+#10
+# a = int(input("M kiriting sm ga aylantiraman:"))
+# b = a*100
+# print(f"{a}m teng {b}sm")
+
+#11
+# a = int(input("T kiriting kg hisoblayman:"))
+# b = a*1000
+# print(f"{a}t teng {b}kg ga ")
+
+#12
+# a= "dushanba"
+# b = "seshanba"
+# c = "chorshanba"
+# d = "payshanba"
+# e = "juma"
+# f = "shanba"
+# g = "yakshanba "
+#
+# i = int(input("Hafta kuni:"))
+# if i == 1:
+#     print(a)
+# elif i == 2:
+#     print(b)
+# elif i == 3:
+#     print(c)
+# elif i == 4:
+#     print(e)
+# elif i == 5:
+#     print(e)
+# elif i == 6:
+#     print(f)
+# else:
+#     print(g)
+
+
+# 13
+# a = "Yanvar"
+# b = "Fevral"
+# c = "mart"
+# d = "aprel"
+# e = "may"
+# f = "iyyun"
+# g = "iyyul"
+# h = "avgust"
+# i = "sentyabr"
+# j = "oktyabr"
+# k = "noyabr"
+# l = "dekabr"
+# m = int(input("Oyni tanlang:"))
+# if m==1:
+#     print(a)
+# elif m==2:
+#     print(b)
+# elif m==3:
+#     print(c)
+# elif m==4:
+#     print(d)
+# elif m==5:
+#     print(e)
+# elif m==6:
+#     print(f)
+# elif m==7:
+#     print(g)
+# elif m==8:
+#     print(h)
+# elif m==9:
+#     print(i)
+# elif m==10:
+#     print(j)
+# elif m==11:
+#     print(k)
+# else:
+#     print(l)
+#
+
+
+# 14
+# a = "Bahor"
+# b = "Yoz"
+# c = "kuz"
+# d = "qish"
+# m = int(input("Fasl raqaqamini kiriting:"))
+# if m == 1:
+#     print(a)
+# elif m == 2:
+#     print(b)
+# elif m == 3:
+#     print(c)
+# else:
+#     print(d)
+#
+
+# 15 Buni qila olmadim
+# a = int(input("Biror son kiriting:"))
+# if a/2:
+#     print(f"{a} son juft son")
+#
+# else:
+#     print(f"{a} son toq son")
+
+
+
+# 16
+# a = int(input("To'g'ilgan yiling gizni kiriting:"))
+# b = 2023
+# v = b-a
+# print(v)
+
+# 18
+# a = int(input("Aylananing radyusini kiriting:"))
+# b = a*2
+# print(f"{a} dyametr {b}")
+
+
+# 19
+# a = input("isming gizni kiriting:")
+# print(f"Assalomu alaykum hurmatli {a}")
+
+
+# 20
+# a = int(input("Istalgan son kiriting:"))
+# b = a-a
+# print(f"sizniki {b}")
+
+
+# 21
+# a = int(input("Istalgan sonni kiriting:"))
+# if a>0:
+#     print("bu sin 0 dan katta")
+# elif a<0:
+#     print("bu son 0dan kichik ")
+# elif a==0:
+#     print("bu sin 0ga teng ")
+# else:
+#     print("ERROR")
+
+
+
+# 22
+
+
+# A = int(input("10gacha bulgan son kiriting:"))
+# if A>10:
+#     print("Bu son buladi ")
+# elif A<10:
+#     print("ERROR")
+# else:
+#     print("Bu ham buladi ")
+
+# 23
+# a = int(input("Yoshni kiriting:"))
+# if a>10:
+#     print("sizga ruxsat ")
+# elif a<10:
+#     print("Error")
+# else:
+#     print("ruxsat")
+
+# 24
+# a = int(input("Son1 ni kiriting:"))
+# b = int(input("son2 ni kiriting:"))
+# if a>b:
+#     print("1-son katta ")
+# elif a<b:
+#     print("2-son katta")
+# else:
+#     print("teng")
+
+
+
+
+# 25
+# a = int(input("Bo'yinggizni kiriting:"))
+# if a>170:
+#     print("siz armyaga keta olasiz")
+# elif a<170:
+#     print("siz armyaga keta olmaysiz")
+# else:
+#  print("siz keta olasiz")
+
+# 26
+# a = input("isminggizni kiriting ")
+# if a == "Asadbek":
+#     print("Siz IT parkda o'qiysiz")
+# else:
+#     print("Siz IT parkda o'qimaysiz")
+
+
+# 27
+
+
---
+++ dars2.py
@@ -0,0 +1,67 @@
+# List
+
+
+# List - []
+
+
+
+# a = ['Olma','Olcha',"101010",'Banan', "olxo'ri"]
+# print(a[-1])
+
+
+
+# viloyat=["Qashqadaryo","Surxandaryo","Xorazim","Navoyi","Jizax","Toshkent","Namangan","Andijon","Farg'ona","Sirdaryo","Samarqant","Buxoro"]
+# print(f" {viloyat[3]} {viloyat[4]} {viloyat[5]} {viloyat[6]} {viloyat[7]} {viloyat[8]}")
+
+# listda ma'lumotni oraliq ko'rinishida chiqarish uchun : shu belgidan foydalanamiz
+# print(f"{viloyat[3:9]}")
+
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:])
+#
+# 3
+# a = ["Aziza","Azamat","sardor","Dilshot","Farruh","Olim"]
+# print(a[2:5])
+
+# 4
+# a = ["Olma", "Anor","Olcha","Anjir","Shaftoli"]
+# print(a[:4])
+
+
+# 5
+# a = ["IOS", "Androit", "Macbook", "Redmi", "Samsung"]
+# print(a[3:])
+
+# 7
+# a = ["Aziz","Azamat", "Sardor" , "Dilshod" , "Farruh", "Olim"]
+# print(a[2:5])
+
+
+
+# 8
+# a = ["qarshi","Toshkent", "Samarqant", "Buxoro","Navoiy"]
+# print(a[2:])
+
+
+# 9
+# a = [input("Shaharlarni kiriting:") , input("Shahar2:")]
+# print(a)
+
+# 10
+# a = ["asdasdsds","asdadasda"]
+# print(type(a))
+
+# 14
+# a = ["Qarshi", "Toshkent", "Samarqant" , "Buxoro", "Navoi"]
+# print(a[1::2])
+
+# a = ["Qarshi", "Toshkent", "Samarqant" , "Buxoro", "Navoi"]
+# print(a[::-1])
+
+
+
+# a = ["Qarshi", "Toshkent", "Samarqant" , "Buxoro", "Navoi"]
+# print(a[1][-1])
+
