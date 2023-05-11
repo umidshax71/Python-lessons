79fe135e2de11a7c97ceaaeb7a6c5431d4f3a390
umidshax71 <ismoilovumid609@gmail.com>
2023-05-11 10:02:12 +0300

    deploy

---
+++ dars3.py
@@ -0,0 +1,12 @@
+# mavzu= takrorlanish operatorlari
+# takrorlanish operatori bu bir nechta obeyiktlarni bitta uzgaruvchi orqali amallar bajarish uchun ishlatiladign opiratorlar
+# for operatorinign belgilanishi (for)
+# namuna
+
+
+
+# shahar = ("DUbai","New York", "Manhattan", "Toshkent")
+# for i in shahar:
+#     print("Salom",i)
+
+
