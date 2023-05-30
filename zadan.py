50764c4577f91891562c585f1187b3388ef94f7e
umidshax71 <ismoilovumid609@gmail.com>
2023-05-30 09:02:55 +0300

    Deploy

--- zadan.py
+++ zadan.py
@@ -1,4 +1,4 @@
-# a = int(input("Son kiriting :"))
+#1) a = int(input("Son kiriting :"))
 # if a <0:
 #     print("manfi son !")
 # else:
@@ -8,5 +8,31 @@
 
 
 
-a = int(input("son kiriting:"))
+# 2)3)a = int(input("son kiriting:"))
+# if a %2==0:
+#     print("Juft son")
+#
+# else:
+#     print("toq son")
+
+# 4)a = int(input("Son1 ni kiritng:"))
+# b = int(input("Son2 ni kiritng:"))
+# if a >2:
+#     print("2dan katta")
+# elif a<=3:
+#     print("3 DAn kichik yoki teng ")
+
+
+
+#6) a = int(input("Son1 ni kiritng:"))
+# b = int(input("Son2 ni kiritng:"))
+# c = int(input("Son3 ni kiritng:"))
+# if a<=b:
+#     print(b,"katta")
+# elif b<=c:
+#     print(c,"Katta ")
+# elif a <= c:
+#     print("kjkjkjkjkjk")
+
+
 
